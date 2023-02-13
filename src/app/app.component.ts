6a417802ad5d1b6839d0e09400c58bf3f96c8651
saurabhgrewal718 <saurabhgrewal718@gmail.com>
2023-02-13 16:13:38 +0530

    gauge chart added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gauge-charts';
+export class AppComponent implements OnInit{
+
+  width = 600;
+  height = 400;
+  type = "angulargauge";
+  dataFormat = "json";
+  dataSource = data;
+  
+  ngOnInit(): void {
+    console.log('dfgf');
+    ;
+  }
+
 }
+
+
+const data = {
+  chart: {
+    caption: "Nordstorm's Customer Satisfaction Score for 2017",
+    lowerlimit: "0",
+    upperlimit: "100",
+    showvalue: "1",
+    numbersuffix: "%",
+    theme: "fusion",
+    showtooltip: "0"
+  },
+  colorrange: {
+    color: [
+      {
+        minvalue: "0",
+        maxvalue: "50",
+        code: "#F2726F"
+      },
+      {
+        minvalue: "50",
+        maxvalue: "75",
+        code: "#FFC533"
+      },
+      {
+        minvalue: "75",
+        maxvalue: "100",
+        code: "#62B58F"
+      }
+    ]
+  },
+  dials: {
+    dial: [
+      {
+        value: "81"
+      }
+    ]
+  }
+};
+
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FusionChartsModule } from "angular-fusioncharts";
+
+// Import FusionCharts library and chart modules
+import * as FusionCharts from "fusioncharts";
+import * as charts from "fusioncharts/fusioncharts.charts";
+import * as FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Pass the fusioncharts library and chart modules
+FusionChartsModule.fcRoot(FusionCharts, charts, FusionTheme);
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
